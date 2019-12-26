a9ff2608cad5d79a9af1e42ac54a9e7f7da72294
Duncan Heffron <dschriel@hotmail.com>
2019-12-26 21:51:33 +0100

    Updates

---
+++ precache-manifest.0d363137bd291e0da6db1d87a22bde6b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c65c0297f8f1b880f1e115fa434aab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "803893be079559aeb535",
+    "url": "/static/css/main.646e0fe6.chunk.css"
+  },
+  {
+    "revision": "f1f12cdb333d15e59e85",
+    "url": "/static/js/2.39280c1d.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/static/js/2.39280c1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "803893be079559aeb535",
+    "url": "/static/js/main.9e9288e1.chunk.js"
+  },
+  {
+    "revision": "06ef1710aaaf492ab761",
+    "url": "/static/js/runtime-main.4eacaa74.js"
+  },
+  {
+    "revision": "4c99316948957fb169ef65891c8fc53e",
+    "url": "/static/media/duncan-brand-white.4c993169.svg"
+  },
+  {
+    "revision": "2694c982e1d8442103d0143f0e70f235",
+    "url": "/static/media/duncan-brand.2694c982.svg"
+  },
+  {
+    "revision": "6637e20d64b2a7bb890c89c06ffb9b0a",
+    "url": "/static/media/wapen-van-utrecht.6637e20d.svg"
+  }
+]);
